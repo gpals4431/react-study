1a50490a2f2e1b6a37d8a255904926e8d82ad24f
junesoftkorea <ripc@junesoft.net>
2025-07-17 20:41:18 +0900

    module

--- src/index.js
+++ src/index.js
@@ -11,9 +11,14 @@
 // console.log(sub(1, 2));
 
 //ES module 시스템 
-import mul, { add, sub } from "./math.js";
-// import { add, sub } from "./math.js";
+// import mul, { add, sub } from "./math.js";
+// // import { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(1, 2));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
+
+//라이브러리는 경로말고 이름만 
+import randomColor from "randomcolor";
+const color = randomColor();
+console.log(color);
